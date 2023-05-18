1240bb99e23d4a60335d2e648d148018201fa9c4
Jeevanakshay <84185011+Jeevanakshay@users.noreply.github.com>
2023-05-18 16:25:07 +0530

    Delete mongo.py

--- mongo.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://intern_23:intern%40123@192.168.0.220:2717/interns_b2_23")
-# Creating database
-db = client.interns_b2_23
-collection_grocery = db.jeevan
-
-
-
